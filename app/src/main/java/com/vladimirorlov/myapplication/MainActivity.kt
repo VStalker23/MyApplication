3715eca5166fe05958c6390cb7fd741018fbea0c
VStalker23 <vovaorlov15@gmail.com>
2022-08-25 17:58:27 +0300

    add all files to git

--- app/src/main/java/com/vladimirorlov/myapplication/MainActivity.kt
+++ app/src/main/java/com/vladimirorlov/myapplication/MainActivity.kt
@@ -2,23 +2,31 @@ package com.vladimirorlov.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("HackerU","onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun getMyName(): String {
         var name: String = "Vladimir"
-        name = "Dani"
         return name
     }
 
     fun onClick(view: View) {
         val textView: TextView = findViewById<TextView>(R.id.hello_text)
-        textView.text = "Hello ${getMyName()} Good to see you in Android Studio for first time!"
+        val editTextCon = getEditText()
+        textView.text = "Hello ${editTextCon} Good to see you in Android Studio for first time!"
+    }
+
+    fun getEditText(): String {
+        val editText: EditText = findViewById(R.id.edit_text_first)
+        return editText.text.toString()
     }
 }
