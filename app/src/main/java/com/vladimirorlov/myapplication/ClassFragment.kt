f9060d215dd9e57efe10c6f49a05b8b52f64f7e5
VStalker23 <vovaorlov15@gmail.com>
2022-09-05 17:57:18 +0300

    add all files from lesson 3 to git

--- app/src/main/java/com/vladimirorlov/myapplication/ClassFragment.kt
+++ app/src/main/java/com/vladimirorlov/myapplication/ClassFragment.kt
@@ -1,5 +1,23 @@
 package com.vladimirorlov.myapplication
 
-class ClassFragment {
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class ClassFragment : Fragment(R.layout.class_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val personTextView =  activity?.findViewById<TextView>(R.id.fragment_class_details)
+        val personImage =  activity?.findViewById<ImageView>(R.id.fragment_class_image)
+
+        val name = requireArguments().getString("name")
+        val image = requireArguments().getInt("image")
+
+        personTextView?.text = name.toString()
+        personImage?.setImageResource(image)
+    }
 
 }
